15ddf97349498a6c2400a4c57fb94dda295949a7
Izaskun Peña <ipena@irontec.com>
2017-10-25 12:17:11 +0200

    Added getAuthCodes method.

--- hub.js
+++ hub.js
@@ -8,7 +8,8 @@
   /** @ngInject */
   function hub($window, $timeout) {
 
-    var methods = ['init', 'resume', 'notifiyEnviroment', 'menuHeaderAction', 'menuItemAction', 'menuOpened', 'menuClosed', 'backButton', 'headerLeftButtonAction', 'headerRightButtonAction', 'showPushNotification'];
+    var methods = ['init', 'resume', 'notifiyEnviroment', 'menuHeaderAction', 'menuItemAction', 'menuOpened', 'menuClosed',
+                   'backButton', 'headerLeftButtonAction', 'headerRightButtonAction', 'showPushNotification', 'getAuthCodes'];
 
     /**
      * Si estamos en una navegador, nos ponemos  la escucha de mensajes (anvil)
