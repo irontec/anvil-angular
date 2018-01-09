07e55b4e37cc4572c8d8cee1e0432675ba74a5d3
Izaskun Peña <ipena@irontec.com>
2018-01-09 16:22:19 +0100

    Task to generate the min version of the bundle.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/**
+ * Created by ipena on 9/01/18.
+ */
+
+const webpack = require('webpack');
+const path = require('path');
+
+
+module.exports = {
+    entry: './index.js',
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'angular-anvil.min.js',
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({ minimize: true })
+    ]
+};
